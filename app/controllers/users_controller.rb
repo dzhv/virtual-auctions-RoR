481043f0949485f9a2a712288ea33ff7d3e89954
dzhv <dz.vysniauskas@gmail.com>
2016-12-02 13:07:17 +0200

    First snipets of web UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def sign_up
+  end
+
+  def log_in
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/sign_up', as: 'sign_up'
+  get 'users/log_in', as: 'log_in'
+
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
